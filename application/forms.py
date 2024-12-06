6ba24649851c1754a1ef860a0c6feb9e240d0737
Bicko-Laura <laura.m.bicko@gmail.com>
2024-12-05 23:11:15 -0800

    added new files

--- application/forms.py
+++ application/forms.py
@@ -12,7 +12,20 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
-        widgets = {}
+        widgets = {
+
+            'full_name': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Enter your full name'}),
+            'contact_number': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Add your contact number'}),
+            'email': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Enter your email'}),
+            'checkin_date': forms.TextInput(attrs={'class': 'form-control','placeholder': 'DD/MM/YYYY'}),
+            'checkout_date': forms.TextInput(attrs={'class': 'form-control','placeholder': 'DD/MM/YYYY'}),
+            'number_of_adults': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Add adults travelling'}),
+            'number_of_children': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Add children travelling'}),
+            'trip_preferences': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Enter your trip preferences'}),
+
+
+
+        }
 
 
 
